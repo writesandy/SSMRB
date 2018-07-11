6ffc9bd6835f70dc88757a80149aa1af25a87b81
Koch <kochm@vikings.nfl.net>
2018-07-11 08:35:27 -0500

    trying the .gitignore one more time

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 
 import ArtistThumbnail from './components/ArtistThumbnail/ArtistThumbnail';
-=======
 import ImageUpload from './components/ImageUpload'
 import GalleryComponent from './components/Gallery'
->>>>>>> 79807c12ceb5cdac89aa81328622e6a3c1d92736
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <ArtistThumbnail />
-=======
         <ImageUpload />
         <GalleryComponent />
->>>>>>> 79807c12ceb5cdac89aa81328622e6a3c1d92736
       </div>
     );
   }
