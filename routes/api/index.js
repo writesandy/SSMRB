fa841b85bee7842d648d1fd10a4c3471eda298e7
bvreeman <bvreeman@gmail.com>
2018-07-10 21:49:14 -0500

    committing to pull

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const artistsRoutes = require("./artists");
-
+const account = require("./account/signin.js")
 // Article routes
 router.use("/saved", artistsRoutes);
+
+router.use("/account", account)
 
 module.exports = router;
