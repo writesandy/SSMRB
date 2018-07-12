5b5f7dda622aa1e6dc4d2b8602748e21775cd6e1
Koch <kochm@vikings.nfl.net>
2018-07-12 13:06:26 -0500

    fixed font size & spacing // no image background color added // LESS NOT in use

--- client/src/components/ArtistThumbnail/ArtistThumbnail.js
+++ client/src/components/ArtistThumbnail/ArtistThumbnail.js
@@ -12,12 +12,12 @@ class ArtistThumbnail extends React.Component {
         return (
             <div className="artist-thumbnail-container">
               {this.state.Artists.map(artist => (
-                <div className="artist-thumbnail col-lg-3 col-sm-6 col-xs-12" key={artist.id}>
-                    <img className="artist-thumbnail-image" src={artist.image} alt="Nature" />
+                <div className="artist-thumbnail col-lg-3 col-md-4 col-sm-6 col-xs-12" key={artist.id}>
+                    <img className="artist-thumbnail-image" src={artist.image} alt="" />
                     <div className="artist-thumbnail-text-block">
                         <h4>{artist.first} {artist.last}</h4>
                         <h6>{artist.title}</h6>
-                        <div className="artist-thumbnail-social">
+                        <div>
                             <a href={artist.instagram} class="fa fa-instagram"></a>
                             <a href={artist.twitter} class="fa fa-twitter"></a>
                             <a href={artist.linkedin} class="fa fa-linkedin"></a>
