51c74b03854b43e6e57a165aa0cda4bf5ec7b7a9
Koch <kochm@vikings.nfl.net>
2018-07-13 11:15:07 -0500

    Artist Profiles | gallery grid/thumbnails done + modal styled + opens/closes BUT does not display clicked image

--- client/src/pages/ArtistProfiles/ArtistProfiles.js
+++ client/src/pages/ArtistProfiles/ArtistProfiles.js
@@ -24,7 +24,8 @@ class ArtistProfiles extends React.Component {
       
     handleCloseModal () {
     this.setState({ showModal: false });
-    }
+    };
+
     
     render () {
         return (
@@ -34,16 +35,19 @@ class ArtistProfiles extends React.Component {
             {/* ArtistBio */}
                 <h2>Artist Image and Bio</h2>
             {/* Gallery */}
-                <div>
+                <div id="art-gallery">
                     {this.state.Artists[0].art.map ((art, index) => {
-                            return (<img className="art col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12" src={art} id={index} onClick={this.handleOpenModal}/>)
+                            return (
+                                <div key={index} className="art col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12"  onClick={this.handleOpenModal} style={{height:200}}>
+                                    <img className="art-img" src={art} />
+                                </div>
+                            )
                     })}
                 </div>
                 {/* Art Feature Modal */}
-                <ReactModal 
-                isOpen={this.state.showModal}
-                >
-                    <button onClick={this.handleCloseModal}>Close Modal</button>
+                <ReactModal isOpen={this.state.showModal}>
+                    <img id="feature-image" src="https://images.pexels.com/photos/900102/pexels-photo-900102.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+                    <button className="close-btn" onClick={this.handleCloseModal}><i class="fa fa-close"/></button>
                 </ReactModal>
              {/* Footer */}
             </div>            
