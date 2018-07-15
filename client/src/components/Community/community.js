02e817539e938f59c9548797ff75c5d7f2171213
Koch <kochm@vikings.nfl.net>
2018-07-15 14:44:26 -0500

    trying to fix float left issue

--- client/src/components/Community/community.js
+++ client/src/components/Community/community.js
@@ -7,7 +7,7 @@ import beardImage from './Image.Beard.png';
 
    
 const Community = () => (
-  <div class='col-sm-6'>Flavor Text About our Project. 
+  <div class='flavor col-sm-6'>Flavor Text About our Project. 
   Come see our favorite artists and their projects. 
   These are people who are really talented and have a community to share. 
   <img src={beardImage} className="beard-image" alt="beard-image" />
