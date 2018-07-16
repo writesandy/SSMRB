28ace2c80f922960386c8514afe8a504d57a8a4e
Sandra Peterson <writesandy@gmail.com>
2018-07-15 22:00:36 -0500

    seeds files

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(routes)
 mongoose.Promise = global.Promise;
 
 // Connect to the Mongo DB
-const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/login_demo_db";
+const dbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/artist_db";
 
 mongoose.connect(dbUri).then(() => console.log('connected to DB!')).catch((err) => console.log(err));
 
