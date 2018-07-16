898fcbc4c5727093ac073d75ac3519ae6ee89e34
Koch <kochm@vikings.nfl.net>
2018-07-15 19:25:06 -0500

    BOOTSTRAP FIX: cdn v4.0.0  /  npm i boostrap@3 for v3.3.7  /  npm uninstall react-bootstrap & delete corresponding line in index.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,11 +3,17 @@ import './Home.css'
 
 // Components
     import About from '../../components/About'
+    import Carousel from "../../components/Carousel"
 
-const Home = props => (
+class Home extends React.PureComponent {
+    render() {
+        return (
             <div>
+                {/* <Carousel fluid={true}/> */}
                 <About />
-            </div>
+            </div>    
         )
+    }
+}
 
 export default Home;
