472fda8ea2b91e14861ff8fd500d950ae1030805
Sandra Peterson <writesandy@gmail.com>
2018-07-17 21:12:40 -0500

    updates to routes

--- client/src/components/ArtistThumbnail/ArtistThumbnail.js
+++ client/src/components/ArtistThumbnail/ArtistThumbnail.js
@@ -14,7 +14,7 @@ class ArtistThumbnail extends React.PureComponent {
 
       componentDidMount() {
         console.log('it mounted')
-        API.getArtists();
+        API.getArtists().then(res=> this.setState({Artists: res.data}))
         //.then 
     }
 
@@ -25,7 +25,7 @@ class ArtistThumbnail extends React.PureComponent {
               {this.state.Artists.map(artist => (
                 <div className="artist-thumbnail-container col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">  
                     <div className="artist-thumbnail" key={artist.id}>
-                        <img className="artist-thumbnail-image" src={artist.image} alt="" />
+                        <img className="artist-thumbnail-image" src={artist.profilePhoto} alt="" />
                         <div className="artist-thumbnail-text-block">
                             <h4 className="artistName">{artist.first} {artist.last}</h4>
                             <h6 className="artistTitle">{artist.title}</h6>
