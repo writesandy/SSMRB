ec0ecd23586a0e26ae114e48932a59705fea30e3
Rikki Honnold <rikkihon@gmail.com>
2018-07-19 19:15:03 -0500

    An update

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -23,25 +23,17 @@ const About = props => (
             </div>
             <div className="about-us-2 col-lg-4 col-md-4 col-sm-12">
                 <p className="about-paragraph">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nunc tristique vel turpis ut ullamcorper. Integer ante elit, tempor et ante in, elementum sagittis nunc.
-                    Donec lobortis massa et nunc placerat, at mattis nisi sollicitudin.
-                    liquam blandit vulputate libero at posuere.
-                    Nunc varius orci sed leo dapibus, sit amet facilisis nunc viverra.
-                    Duis sit amet ullamcorper mi. Cras faucibus congue nibh, ac suscipit justo laoreet sed.
-                    Etiam euismod leo eu tellus rutrum, a mollis mauris venenatis. In in facilisis dolor. Praesent id iaculis massa. Donec non maximus neque.
+                    Welcome to McMinneville, Oregon's finest Artist Community! We believe that art is the center of cultivating a positive community experience. 
+                    Art heals, unifies, and spotlights the best aspects in of our human lives. 
                     </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nunc tristique vel turpis ut ullamcorper.
-                    Integer ante elit, tempor et ante in, elementum sagittis nunc.
-                    Donec lobortis massa et nunc placerat, at mattis nisi sollicitudin.
+                    We'd love to have you visit our galleries, join one of our featured educational opportunities, and come to our demonstrations of our finest work examples.
                     </p>
             </div>
 
             <div className="services-offerings col-lg-5 col-md-5 col-sm-12">
-                One of two columns
-                </div>
+              Artistic expression can be a powerful means of personal transformation and emotional and spiritual healing.
+            </div>
         </div>
     </div>
 
