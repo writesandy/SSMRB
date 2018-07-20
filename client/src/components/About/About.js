c55b31caceab12223e464b30fa6bb9bed39cd2f8
bvreeman <bvreeman@gmail.com>
2018-07-19 21:22:36 -0500

    troubleshooting

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -18,17 +18,18 @@ const About = props => (
                     Nunc varius orci sed leo dapibus, sit amet facilisis nunc viverra.
                     Duis sit amet ullamcorper mi. Cras faucibus congue nibh, ac suscipit justo laoreet sed.
                     Etiam euismod leo eu tellus rutrum, a mollis mauris venenatis. In in facilisis dolor. Praesent id iaculis massa. Donec non maximus neque.
-                    </p>
-                <p className="about-image">
+                </p>
+                <div className="about-image">
                     <img src="https://i.imgur.com/fUZx5Gz.jpg" className="img-fluid" alt="about us profile image" />
+                </div>
                 <p className="about-paragraph">
                     We are an artist-owned and artist-run community. We cultivate our community of artists and feature a new artist profile each month.
                     Our mission is to provide a space for each artist to practice their craft and present to the public.
                     Artists will be available for commissioned pieces. 
                     </p>
-                <p className="about-image">
+                <div className="about-image">
                     <img src="https://i.imgur.com/9lJaU1c.jpg" className="img-fluid" alt="Responsive" />
-                </p>
+                </div>
             </div>
             <div className="about-us-2 col-lg-4 col-md-4 col-sm-12">
                 <p className="about-paragraph">
