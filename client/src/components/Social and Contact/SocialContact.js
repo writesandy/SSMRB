b295d63e720152bec9242aa548f2064adfd5355d
Koch <kochm@vikings.nfl.net>
2018-07-21 13:01:26 -0500

    fixed social icons on artist profile page

--- client/src/components/Social and Contact/SocialContact.js
+++ client/src/components/Social and Contact/SocialContact.js
@@ -4,10 +4,10 @@ import "./SocialContact.css";
 
 const SocialContact = props => (
     <div className="socialContact">
-        <a href={props.instagram} className="fa fa-instagram socialContactMin col-md-1 col-sm-1 col-xs-1">Instagram</a>
-        <a href={props.twitter} className="fa fa-twitter socialContactMin col-md-1 col-sm-1 col-xs-1">Twitter</a>
-        <a href={props.linkedin} className="fa fa-linkedin socialContactMin col-md-1 col-sm-1 col-xs-1">LinkedIn</a>
-        <a href={props.website} className="fa fa-globe socialContactMin col-md-1 col-sm-1 col-xs-1"></a>
+        <i href={props.instagram} className="fa fa-instagram socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
+        <i href={props.twitter} className="fa fa-twitter socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
+        <i href={props.linkedin} className="fa fa-linkedin socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
+        <i href={props.website} className="fa fa-globe socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
         <a className="fa col-md-8 col-sm-8 col-xs-8 socialContactMax">{props.email}</a>
     <ReactTooltip />
     </div>
