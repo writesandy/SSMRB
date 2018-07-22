6e39f964935d0b8ee66a0230a237499f3a9fe20f
bvreeman <bvreeman@gmail.com>
2018-07-21 23:26:34 -0500

    working through the database piece and accessing the urls. Just finished putting the urls into an array

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HeaderNav from './components/HeaderNav';
 import Footer from './components/Footer/Footer';
+import firebase from 'firebase';
 
 // Pages
 import Home from './pages/Home'
@@ -14,13 +15,20 @@ import GalleryComponent from './components/Gallery'
 // import About from './components/About'
 import ArtistProfiles from './pages/ArtistProfiles/ArtistProfiles';
 
-// require('dotenv').config()
-
-
-
 class App extends PureComponent {
+  componentWillMount() {
+    var config = {
+        apiKey: process.env.REACT_APP_APIKEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        databaseURL: process.env.REACT_APP_URL,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+      };
+    //   console.log(config)
+      firebase.initializeApp(config);
+  }
   render() {
-    console.log(process.env)
     return (
       <Router>
         <div id="page">
--- client/src/components/ImageUpload/ImageUpload.js
+++ client/src/components/ImageUpload/ImageUpload.js
@@ -4,32 +4,19 @@ import './ImageUpload.css';
 // import { List, ListItem } from "../../components/List";
 import firebase from "firebase";
 import FileUploader from "react-firebase-file-uploader";
-import CustomUploadButton from 'react-firebase-file-uploader/lib/CustomUploadButton';
+// import CustomUploadButton from 'react-firebase-file-uploader/lib/CustomUploadButton';
 
 class ImageUpload extends PureComponent {
     state = {
       name: '',
-      file: null,
-      files: [],
+    //   file: null,
+    //   files: [],
       username: "",
-      avatar: "",
+      imageName: "",
       isUploading: false,
       progress: 0,
-      avatarURL: ""
+      imageURL: ""
     }
-
-  componentWillMount() {
-    var config = {
-        apiKey: process.env.REACT_APP_APIKEY,
-        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-        databaseURL: process.env.REACT_APP_URL,
-        projectId: process.env.REACT_APP_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
-      };
-      console.log(config)
-      firebase.initializeApp(config);
-  }
 
   handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
   
@@ -40,34 +27,49 @@ class ImageUpload extends PureComponent {
     console.error(error);
   };
 
-  handleUploadSuccess = filename => {
-    this.setState({ avatar: filename, progress: 100, isUploading: false });
-    firebase
-      .storage()
-      .ref("images")
-      .child(filename)
-      .getDownloadURL()
-      .then(url => this.setState({ avatarURL: url }))
-      .then(alert("Succesfully uploaded"))
-      console.log(firebase.storage().ref("images").child(filename).getDownloadURL())
-  };
+  databasePush = () => {
+    let itemsRef = firebase.database().ref('ImageData/')
+    console.log(this.state);
+    let updates = {
+        url: this.state.imageURL,
+        name: this.state.imageName
+    }
+    itemsRef.push(updates);
+}
+
+    handleUploadSuccess = filename => {
+        this.setState({ imageName: filename, progress: 100, isUploading: false });
+        firebase
+            .storage()
+            .ref("images")
+            .child(filename)
+            .getDownloadURL()
+            .then(url => {
+                this.setState({ 
+                    imageURL: url,
+                    imageName: filename 
+                })
+            }).then(this.databasePush())
+            // console.log(firebase.storage().ref("images").child(filename).getDownloadURL())
+    };    
 
   render() {
     return (
       <div>
         <form>
-          <label>Avatar:</label>
+          <label>Upload an Image:</label>
           {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
-          {this.state.avatarURL && <img src={this.state.avatarURL} />}
+          {this.state.imageURL && <img alt={this.state.filename} src={this.state.imageURL} />}
           <FileUploader
             accept="image/*"
-            name="avatar"
+            name="imageName"
             randomizeFilename
             storageRef={firebase.storage().ref("images")}
             onUploadStart={this.handleUploadStart}
             onUploadError={this.handleUploadError}
             onUploadSuccess={this.handleUploadSuccess}
             onProgress={this.handleProgress}
+            // onPushtoDatabase={this.handlePushToDatabase}
           />
         </form>
       </div>        
