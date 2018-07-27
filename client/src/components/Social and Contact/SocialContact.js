b03a710927b568ecd3db3c78b73bcd49166ae0f9
Sandra Peterson <writesandy@gmail.com>
2018-07-27 07:49:34 -0500

    updating artist bio links - must be a tags to work as links

--- client/src/components/Social and Contact/SocialContact.js
+++ client/src/components/Social and Contact/SocialContact.js
@@ -4,10 +4,10 @@ import "./SocialContact.css";
 
 const SocialContact = props => (
     <div className="socialContact">
-        <i href={props.instagram} className="fa fa-instagram socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
-        <i href={props.twitter} className="fa fa-twitter socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
-        <i href={props.linkedin} className="fa fa-linkedin socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
-        <i href={props.website} className="fa fa-globe socialContactMin col-md-1 col-sm-1 col-xs-1"></i>
+        <a href={props.instagram} className="fa fa-instagram socialContactMin col-md-1 col-sm-1 col-xs-1"></a>
+        <a href={props.twitter} className="fa fa-twitter socialContactMin col-md-1 col-sm-1 col-xs-1"></a>
+        <a href={props.linkedin} className="fa fa-linkedin socialContactMin col-md-1 col-sm-1 col-xs-1"></a>
+        <a href={props.website} className="fa fa-globe socialContactMin col-md-1 col-sm-1 col-xs-1"></a>
         {/* Below: email shouldn't be an a tag but it can't be an i tag -- h6? */}
         <a className="fa col-md-8 col-sm-8 col-xs-8 socialContactMax">{props.email}</a>
     <ReactTooltip />
