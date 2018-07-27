b14701a0868282bc02cb9b876e267709aebec2db
Rikki Honnold <rikkihon@gmail.com>
2018-07-26 19:41:54 -0500

    An update to RikkiNew

--- client/src/components/Social and Contact/SocialContact.js
+++ client/src/components/Social and Contact/SocialContact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactTooltip from "react-tooltip";
 import "./SocialContact.css";
 
 const SocialContact = props => (
--- routes/api/account/signin.js
+++ routes/api/account/signin.js
@@ -89,6 +89,7 @@ router.route("/signupprofile").post( (req, res, next) => {
   let { email, first, last, InstagramHandle, TwitterHandle, title, website, artistBio, LinkedIn, artistBoolean } = body;
 
   email = email.toLowerCase();
+//.match will match a string to a regular expression 
   //put regex expression here looking for full email address 
   //(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
   //
